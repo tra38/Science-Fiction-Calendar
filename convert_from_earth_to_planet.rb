bf4c7830a7be4f9b15ba40df834af9ef75719f41
tra38 <tra38@nau.edu>
2015-05-03 17:22:56 -0500

    Add converter from Earth day/year units to Planet day/year units

---
+++ convert_from_earth_to_planet.rb
@@ -0,0 +1,23 @@
+def convert_earth_to_planet
+	puts "Sometimes, it's very hard to really remember a planet's unique day/hour unit."
+	puts "So this convertor is here for you."
+	puts "First, how many Earth days are in a Planet Year?"
+	earthdaysinplanetyear = gets.chomp.to_f
+	puts "Now, how many Earth Days are in a Planet Day?"
+	earthdaysinplanetday = gets.chomp.to_f
+	puts "There are #{earthdaysinplanetyear/earthdaysinplanetday} days are in one Planet Year!"
+	puts "There are #{earthdaysinplanetday * 24} hours in one Planet Day!"
+end
+
+convert_earth_to_planet
+##Convert From Earth Data to Planet Data
+### earthdays    	PlanetDay 	planetdays
+### -------    	 *	 ------- =   ---------
+### planetyear     earthDay    planetyears
+
+#Now. This only tells me planetdays. I want planet hours though.
+
+##I know one earthDay is equal to 24 hours.
+## X EarthDay    24 Hours
+## --------  * -------  =  Hours/PlanetDay
+## PlanetDay   1 EarthDay
